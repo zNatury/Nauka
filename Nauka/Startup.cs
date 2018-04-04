10f15f8b31af582432a28fd2976e05496ed17fc9
jakub.lipka <jakub.lipka@PLWAW230>
2018-04-04 18:46:46 +0200

    Add project files.

---
+++ Nauka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nauka.Startup))]
+namespace Nauka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
